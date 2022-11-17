66858918dfdbc3cd44ab3e98afef527ad2436d8a
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2022-11-17 23:06:18 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+
+function go(){
+    let word = document.getElementById('input').value;
+   
+   
+   /* Fetching API */
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
+.then(function(response){
+if(response.status == 404){
+alert("No Word Found");
+}
+else{
+    return response.json();
+}
+})
+.then(function(data){
+    const { definition } = data[0].meanings[0].definitions[0];
+    def1(definition);    
+    def2();
+    /* Show Definition*/
+    
+    document.querySelector("#ans").style.display="block";
+});
+}
+
+
+
+function def1(definition){
+document.getElementById('first').innerText = definition; 
+
+}
+
+
+
+
+function def2(word){
+fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
+.then((response) =>{
+    return response.json();
+})
+.then((data) =>{
+    const { definition } = data[0].meanings[0].definitions[1];
+    document.getElementById('second').innerText = definition; 
+});
+}
